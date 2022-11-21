9893842baa19d50e9a0ff49b7c1fad644105fb29
Talha Noman <talhanoman61@gmail.com>
2022-11-21 09:59:43 +0500

    CORS Handlers
    CORS Enabled in Server

--- go.mod
+++ go.mod
@@ -3,7 +3,9 @@ module main
 go 1.19
 
 require (
+	github.com/felixge/httpsnoop v1.0.1 // indirect
 	github.com/go-sql-driver/mysql v1.6.0 // indirect
 	github.com/google/uuid v1.3.0 // indirect
+	github.com/gorilla/handlers v1.5.1 // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
 )
--- routes/routes.go
+++ routes/routes.go
@@ -11,13 +11,30 @@ import (
 	// "sync"
 	// "github.com/dgrijalva/jwt-go"
 	"github.com/gorilla/mux"
+	"github.com/gorilla/handlers"
 
 )
+
+
+
+func Test() {
+	// code to write
+	fmt.Println("In routes")
+}
 
 func HandleRequests() {
 	fmt.Println("In Routes")
 	Router := mux.NewRouter().StrictSlash(true)
-	// Post Routes
+
+	// Where ORIGIN_ALLOWED is like `scheme://dns[:port]`, or `*` (insecure)
+	headersOk := handlers.AllowedHeaders([]string{"X-Requested-With"})
+	originsOk := handlers.AllowedOrigins([]string{"*"})
+	methodsOk := handlers.AllowedMethods([]string{"GET", "HEAD", "POST", "PUT", "OPTIONS"})
+
+	// start server listen
+	// with error handling
+	
+	// Router.HandleFunc("/", controllers.PrintHello)
 	Router.HandleFunc("/user", controllers.InsertUser).Methods("POST")
 	Router.HandleFunc("/education", controllers.InsertEducation).Methods("POST")
 	Router.HandleFunc("/experience", controllers.InsertExperience).Methods("POST")
@@ -40,6 +57,7 @@ func HandleRequests() {
 	Router.HandleFunc("/user", controllers.UpdateUser).Methods("PUT")
 	Router.HandleFunc("/education", controllers.UpdateEducation).Methods("PUT")
 	Router.HandleFunc("/experience", controllers.UpdateExperience).Methods("PUT")
-	Router.HandleFunc("/projects", controllers.UpdateProject).Methods("PUT")
-	log.Fatal(http.ListenAndServe(":8000", Router))
+	Router.HandleFunc("/projects", controllers.UpdateProject).Methods("PUT")	
+	// log.Fatal(http.ListenAndServe(":3000", Router))
+	log.Fatal(http.ListenAndServe(":3000", handlers.CORS(originsOk, headersOk, methodsOk)(Router)))
 }
